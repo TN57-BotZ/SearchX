d53ec199b449aae9df00a2e9155274bc79ea3b56
Rhodes Rhody <85724542+RhodesRhody@users.noreply.github.com>
2021-08-09 09:36:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,9 @@
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
+fi
+
+if [[ -n $JSON_URL ]]; then
+	wget -q $JSON_URL -O /usr/src/app/credentials.json
+fi
+
 python3 -m bot
